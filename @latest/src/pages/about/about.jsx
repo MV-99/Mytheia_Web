58ee273bcaaa7556eab9fcb3ae82d9d133758163
MV99🌙 <maribithvallecillo@gmail.com>
2025-10-13 17:12:39 +0000

    guardo denuevo

--- @latest/src/pages/about/about.jsx
+++ @latest/src/pages/about/about.jsx
@@ -1,8 +1,11 @@
-
+import '../../styles/encabezados.css'
 
 export default function About(){
     return(
-
-        <div style={{color:'white'}}>aca se vera mas info sobre la pagina</div>
+      <div className="arriba"><code>ABOUT ME</code>
+        <h5 className="texto">A small introduction to stoicism, discipline and strenght</h5>
+        <img style={{width:'250px',height:'300px'}} src="woman2.jpeg" alt="introduccion"/>
+        </div>
+        
     );
 }
