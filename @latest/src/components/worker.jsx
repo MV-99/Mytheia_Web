298400698c4009e71018dae1194b5e1ce532ebc0
MV99🌙 <maribithvallecillo@gmail.com>
2025-10-14 20:44:35 +0000

    about finish pt1

--- @latest/src/components/worker.jsx
+++ @latest/src/components/worker.jsx
@@ -1,11 +1,12 @@
 
 
-export default function Workers(src, alt,nombre, funcion){
+export default function Workers({src, alt,nombre, funcion, style, texto2}){
 return(
 <div style={{display:'flex',flexDirection:'column'}}>
 <img src={src} alt={alt}/>
-<h4>{nombre}</h4>
+<h4 style={style}>{nombre}</h4>
 <h5>{funcion}</h5>
+<button style={style}>{texto2}</button>
 </div>
 );
 }
